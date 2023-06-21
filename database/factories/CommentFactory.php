e3ca07b142bbc91b57c8ef4db86590845a4ef611
Bouslah Taha <bouslahtaha33@gmail.com>
2023-06-22 08:57:03 +1300

    Factories

---
+++ database/factories/CommentFactory.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Post;
+use App\Models\User;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Comment>
+ */
+class CommentFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'comment' => fake()->sentence(),
+            'post_id' => Post::get('id')->random(),
+            'user_id' => User::get('id')->random(),
+            'created_at' => now()
+        ];
+    }
+}
