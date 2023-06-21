e1f9bbd9f62ca0b319c1eb7ac8410268b9cc4284
Bouslah Taha <bouslahtaha33@gmail.com>
2023-06-22 08:39:51 +1300

    Model's Relationships

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -4,8 +4,22 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Post extends Model
 {
     use HasFactory;
+
+    public function user(): BelongsTo {
+         return $this->belongsTo(User::class);
+    }
+
+    public function Category(): BelongsTo {
+        return $this->belongsTo(Category::class);
+    }
+
+    public function comments(): HasMany {
+        return $this->hasMany(Comment::class);
+    }
 }
