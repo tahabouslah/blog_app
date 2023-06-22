7ddebca68e073c97118532d450909752b2bf4f4a
Bouslah Taha <bouslahtaha33@gmail.com>
2023-06-23 06:55:53 +1300

    solve bootstrap errors

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -1,3 +1,10 @@
+import loadash from 'lodash'
+window._ = loadash
+
+
+import * as Popper from '@popperjs/core'
+window.Popper = Popper
+
 import 'bootstrap';
 
 /**
---
+++ resources/views/posts/index.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Blog</title>
+    @vite(['resources/sass/app.scss', 'resources/js/app.js'])
+</head>
+<body>
+    <div class="container">
+        <div class="btn btn-success">
+            test
+        </div>
+    </div>
+</body>
+</html>
