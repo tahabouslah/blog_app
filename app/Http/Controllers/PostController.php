a5517b16a149462f95dcff966d54befd7867f074
Bouslah Taha <bouslahtaha33@gmail.com>
2023-06-23 05:54:12 +1300

    return posts/index

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Post;
 
 class PostController extends Controller
 {
@@ -11,7 +12,8 @@ class PostController extends Controller
      */
     public function index()
     {
-        //
+        $posts = Post::all();
+        return view("posts.index", compact('posts'));
     }
 
     /**
